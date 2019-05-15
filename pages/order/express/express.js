5ff283edaaa67813fc9771644c78ef4e616eccba
Junbao Chen <Chen.Junbao@outlook.com>
2019-05-15 18:20:55 +0800

    change px to rpx

---
+++ pages/order/express/express.js
@@ -0,0 +1,34 @@
+Page({
+    data: {
+        deliveryInfo: [
+            {
+                'profilePhoto': '/image/好友.png',
+                'name': 'char',
+                'time': '4小时前',
+                'state': '未领取',
+                'description': '有没有人啊，要饿死啦！求带7食堂的照烧鸡排饭！！地址：女子小区6号楼（接头地点私聊叭）'
+            },
+            {
+                'profilePhoto': '/image/好友.png',
+                'name': 'char',
+                'time': '4小时前',
+                'state': '未领取',
+                'description': '有没有人啊，要饿死啦！求带7食堂的照烧鸡排饭！！地址：女子小区6号楼（接头地点私聊叭）'
+            },
+            {
+                'profilePhoto': '/image/好友.png',
+                'name': 'char',
+                'time': '4小时前',
+                'state': '未领取',
+                'description': '有没有人啊，要饿死啦！求带7食堂的照烧鸡排饭！！地址：女子小区6号楼（接头地点私聊叭）'
+            },
+            {
+                'profilePhoto': '/image/好友.png',
+                'name': 'char',
+                'time': '4小时前',
+                'state': '未领取',
+                'description': '有没有人啊，要饿死啦！求带7食堂的照烧鸡排饭！！地址：女子小区6号楼（接头地点私聊叭）'
+            }
+        ]
+    },
+})
