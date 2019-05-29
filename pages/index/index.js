8b0839cb94093e205096bc380143e6cd8542083e
Junbao Chen <Chen.Junbao@outlook.com>
2019-05-29 23:09:21 +0800

    finish search

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,32 @@ var connect = require('../../connect.js');
 
 Page({
   data: {
-    infoList: [],
+    infoList: [
+      {
+        'image': '/image/home.jpg',
+        'time': '4小时前',
+        'state': '已领取',
+        'name': 'char',
+        'tag': '外卖',
+        'description': '有没有人啊，幺二四啦！求带七食堂照烧鸡排饭！地址：女子小区(接头地点私聊把)'
+      },
+      {
+        'image': '/image/home.jpg',
+        'time': '0507 16：00',
+        'state': '--',
+        'name': '套马杆的汉字',
+        'tag': '聊天',
+        'description': '空虚寂寞冷，求小姐姐撩骚'
+      },
+      {
+        'image': '/image/home.jpg',
+        'time': '05-07',
+        'state': '未领取',
+        'name': 'matcha',
+        'tag': '快递',
+        'description': '小南门唯品会的快递那位小哥哥小姐姐或者大国可以帮忙去一下，13：00前哦，谢谢，地址：女子小区(接头地点私聊把)'
+      }
+    ],
     imgUrls: [
       'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
@@ -13,26 +38,6 @@ Page({
     autoplay: true,
     interval: 3000,
     duration: 1000
-  },
-
-  onShow: function (e) {
-
-  },
-
-  navigateToAccept: function (e) {
-    //e.currentTarget.id 为 deliveryInfo 中的id
-    //console.log(e.currentTarget.id);
-    wx.navigateTo({
-      url: '../order/accept/accept'
-    })
-  },
-  navigateToAddFriend: function (e) {
-    //e.currentTarget.id 为 deliveryInfo 中的id
-    console.log(e.currentTarget.id);
-    wx.navigateTo({
-     // url: '../order/addFriend/addFriend'
-      url: '../order/addFriend/addFriend?id=' +e.currentTarget.id
-    })
   },
 
   //跳转下单界面
@@ -57,12 +62,18 @@ Page({
   addFriend: function (options) {
     wx.navigateTo({
       url: '../order/addFriend/addFriend',
-     
     })
   },
 
   onShow:function(){
     connect.changeRecommendPage(this);
+  },
+
+  //搜索信息
+  onSearch:function(e){
+    const info = e.detail;
+    //console.log(this,info);
+    connect.searchRecommendPage(this,info);
   },
 
   //获取用户信息
@@ -84,14 +95,5 @@ Page({
     // 用户点击授权后，这里可以做一些登陆操作
     app.globalData.userInfo = event.detail.event.detail.userInfo;
     connect.initUserData();
-  },
-
-  //下拉刷新
-  onPullDownRefresh: function () {
-    setTimeout(function () {
-      wx.hideNavigationBarLoading() //完成停止加载
-      wx.stopPullDownRefresh() //停止下拉刷新
-    }, 1500);
-    //console.log("下拉刷新")
-  },
+  }
 })
