e95c690ee1eb643dc46a6a1b649c1bc2a2542448
Junbao Chen <Chen.Junbao@outlook.com>
2019-05-29 21:26:22 +0800

    delete add friend modal

--- pages/order/addFriend/addFriend.js
+++ pages/order/addFriend/addFriend.js
@@ -19,24 +19,13 @@ Page({
   },
 
   success:function(){
-      if (this.data.requestInput == '' || this.data.aliasInput  == '') {
-        wx.showToast({
-            title: '存在未填信息',
-            icon: 'none',
-            duration: 1000
-        }, 2000)
-        //console.log('存在未填信息');
-    }
-    else
-    {
-        wx.navigateBack({
-            delta: 1
-        })
-        wx.showToast({
-            title: '提交成功',
-            icon: 'success',
-            duration: 1000
-        }, 2000)
-    }
+    wx.navigateBack({
+        delta: 1
+    })
+    wx.showToast({
+        title: '提交成功',
+        icon: 'success',
+        duration: 1000
+    }, 2000)
   }
 })
